9acc82c9250b7d2e96ed43defa42b553b0464e2a
Gustavo Valentin <gstvalentin@hotmail.com>
2022-04-12 16:00:53 -0300

    Lista de palavras PT-BR

---
+++ words.py
@@ -0,0 +1,13 @@
+from unidecode import unidecode
+
+dicionario = open('br-utf8.txt', 'r', encoding='utf-8').readlines()[0:10] #!! BASTAMTE CUIDADO AO IMPORTAR TODAS AS PALAVRAS, SÃO MAIS DE 250K DE ITENS QUE VOCÊ IRÁ COLOCAR EM SUA LISTA || PARA IMPORTAR TODAS, APAGUE A SEGUINTE PARTE DO CÓDIGO "[0:10]"
+lista_palavras = []
+for d in dicionario:
+    words = unidecode(d.replace('\n', '')) #? Sem acentos e ç
+    #words = d.replace('\n', '')  #? Com acentos e ç
+    lista_palavras.append(words)
+
+"""
+? Palavras importadas da lista da USP Brasil em 12/04/2022 no site "https://www.ime.usp.br/~pf/dicios/index.html"
+? Para atualiza-la, importe um novo arquivo "br-utf8.txt" e substitua o existente nesse diretorio.
+"""
